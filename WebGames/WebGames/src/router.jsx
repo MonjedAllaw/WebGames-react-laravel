b53133e3a11bbdf32e599140f8cb351d51ff0875
Monjed Allaw <allawmonjed@gmail.com>
2023-04-15 19:17:40 +0300

    context-router

---
+++ WebGames/WebGames/src/router.jsx
@@ -0,0 +1,51 @@
+import {createBrowserRouter, Navigate} from 'react-router-dom';
+
+import DefaultLayout from './components/DefaultLayout';
+import GuestLayout from './components/GuestLayout';
+import Login from './views/auth/Login';
+import Signup from './views/auth/Signup';
+import Home from './views/Home';
+import NotFound from './views/NotFound';
+
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<DefaultLayout/>,
+        children: [
+            {
+                path:'/home',
+                element:<Home/>
+            },
+            {
+                path:'/',
+                element:<Navigate to='/home'/>
+            }
+            
+        ]
+    },
+    {
+        path:'/',
+        element:<GuestLayout/>,
+        children: [
+            {
+                path:'/signup',
+                element:<Signup/>
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/',
+                element:<Navigate to='/signup'/>
+            }
+        ]
+    },
+    {
+        path:'*',
+        element: <NotFound/>
+    }
+])
+
+export default router;
